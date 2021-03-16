3b42a471b16c51e9a334fd6adea3bab9d9c203a4
ivannesquick <neskin8@gmail.com>
2021-03-16 23:21:41 +0700

    add service and protocol, fix bug

--- TestApp/MainInteractor/MainInteractor.swift
+++ TestApp/MainInteractor/MainInteractor.swift
@@ -9,25 +9,11 @@
 import Foundation
 
 class MainInteractor: IMainInteractor {
-    let allItem: [ImageInfo] = [
-        ImageInfo(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Mt-St-Greg-RueEcureuils-3.jpg/1024px-Mt-St-Greg-RueEcureuils-3.jpg"),
-        ImageInfo(url: "https://bipbap.ru/wp-content/uploads/2017/04/72fqw2qq3kxh.jpg"),
-        ImageInfo(url: "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"),
-        ImageInfo(url: "https://proprikol.ru/wp-content/uploads/2019/12/privet-krasivye-kartinki-1.jpg"),
-        ImageInfo(url: "https://www.krym4you.com/files/catalog/138/gallery/big//chernoe-more_1435752144.jpg"),
-        ImageInfo(url: "https://s1.1zoom.ru/big0/283/Ships_Sailing_Sea_Sky_534514_1365x1024.jpg")
-    ]
-    //переделать
-    var item: [ImageInfo] = [
-        ImageInfo(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Mt-St-Greg-RueEcureuils-3.jpg/1024px-Mt-St-Greg-RueEcureuils-3.jpg"),
-        ImageInfo(url: "https://bipbap.ru/wp-content/uploads/2017/04/72fqw2qq3kxh.jpg"),
-        ImageInfo(url: "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"),
-        ImageInfo(url: "https://proprikol.ru/wp-content/uploads/2019/12/privet-krasivye-kartinki-1.jpg"),
-        ImageInfo(url: "https://www.krym4you.com/files/catalog/138/gallery/big//chernoe-more_1435752144.jpg"),
-        ImageInfo(url: "https://s1.1zoom.ru/big0/283/Ships_Sailing_Sea_Sky_534514_1365x1024.jpg")
-    ]
+    var imageStubs:IImageStub = ImageStubsService()
+    var item:[ImageInfo] = []
     
     func items() -> [ImageInfo] {
+        item = imageStubs.items()
         return item
     }
     
@@ -36,7 +22,7 @@ class MainInteractor: IMainInteractor {
     }
     
     func refreshAllElements() -> [ImageInfo] {
-        item = allItem
+        item = imageStubs.items()
         return item
     }
     
---
+++ TestApp/Service/IImageStub.swift
@@ -0,0 +1,13 @@
+//
+//  IImageStub.swift
+//  TestApp
+//
+//  Created by Neskin Ivan on 16.03.2021.
+//  Copyright © 2021 Neskin Ivan. All rights reserved.
+//
+
+import Foundation
+
+protocol IImageStub {
+    func items() -> [ImageInfo]
+}
